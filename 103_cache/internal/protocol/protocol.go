6afd15073d89046b2be9ca492aafb8b538498890
Atsushi Toda <todaatsushi0@gmail.com>
2024-08-02 19:41:36 +0100

    Add message shape

--- 103_cache/internal/protocol/protocol.go
+++ 103_cache/internal/protocol/protocol.go
@@ -1,5 +1,7 @@
 package protocol
 
+import "time"
+
 // Version (1B) | Command (1B) | TTL (1B) | Length (2B) | Data (x)
 const VERSION byte = 1
 
@@ -11,3 +13,9 @@ const (
 	Set
 	Update
 )
+
+type Message struct {
+	Cmd     Command
+	Data    []byte
+	Expires time.Time
+}
