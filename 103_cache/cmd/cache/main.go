07a7ed4accc3b66fa4a66a42b0ce480ca36cbb05
Atsushi Toda <todaatsushi0@gmail.com>
2024-08-11 16:17:57 +0100

    Start server

--- 103_cache/cmd/cache/main.go
+++ 103_cache/cmd/cache/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"log"
+	"strings"
+
+	"github.com/todaatsushi/handrolled-cache/cmd/client"
+	"github.com/todaatsushi/handrolled-cache/cmd/server"
+)
 
 func main() {
-	log.Println("Hello, World!")
+	port := flag.Int("p", 420, "Runs on.")
+	cacheSize := flag.Int("c", 0, "Max number of items in cache.")
+	runType := flag.String("type", "", "One of 'SERVER' or 'CLIENT'")
+
+	flag.Parse()
+
+	t := strings.ToLower(*runType)
+
+	switch t {
+	case "server":
+		log.Fatal(server.Start(*port, *cacheSize))
+	case "client":
+		log.Fatal(client.Start(*port))
+	default:
+		log.Fatal("'type' must be one of 'SERVER' or 'CLIENT'.")
+	}
 }
