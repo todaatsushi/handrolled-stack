09148e33a8413961fbe960cdc486452c9cbdde49
Atsushi Toda <todaatsushi0@gmail.com>
2024-11-09 15:09:19 +0000

    Basic hello world server

--- 104_load_balancer/servers/src/servers/main.py
+++ 104_load_balancer/servers/src/servers/main.py
@@ -1,10 +1,18 @@
+from __future__ import annotations
+
 import logging
+import flask
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-def run() -> None:
-    logger.info("Running main function")
+app = flask.Flask(__name__)
+SERVER_ID = 1
 
-if __name__ == '__main__':
-    run()
+
+@app.route("/")
+def home() -> str:
+    logger.info(
+        f"Server #{SERVER_ID} received a request: {flask.request.method} @ {flask.request.url}"
+    )
+    return f"Hello from server #{SERVER_ID}\n"
