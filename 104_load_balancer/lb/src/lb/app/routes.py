aac9c847052ad46c721bff2ffefeb154f59bde7f
Atsushi Toda <todaatsushi0@gmail.com>
2024-11-10 13:09:31 +0000

    Linting + todos

--- 104_load_balancer/lb/src/lb/app/routes.py
+++ 104_load_balancer/lb/src/lb/app/routes.py
@@ -10,11 +10,13 @@ strategy = strategies.get_strategy()
 
 
 def configure_routes(app: flask.Flask) -> None:
+    # Should be any
     @app.route("/")
     def lb() -> flask.Response:
         request_id = uuid.uuid4()
 
         tries = 0
+        response = None
         while tries < MAX_ATTEMPS:
             dest = strategy.get_next()
             response = dest.forward(flask.request, str(request_id))
@@ -22,4 +24,6 @@ def configure_routes(app: flask.Flask) -> None:
                 tries += 1
                 continue
             return response
+
+        assert response
         return response
