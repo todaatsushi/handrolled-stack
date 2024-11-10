8180814c5bf46ead37cd6ec21c5161d4ac168192
Atsushi Toda <todaatsushi0@gmail.com>
2024-11-10 13:00:20 +0000

    Forwards request

--- 104_load_balancer/lb/src/lb/servers.py
+++ 104_load_balancer/lb/src/lb/servers.py
@@ -1,11 +1,36 @@
 import abc
 import dataclasses as dc
+import uuid
+import requests
+
+import flask
 
 
 @dc.dataclass
 class BaseServerConfig(abc.ABC):
     host: str
     port: int
+
+    def forward(
+        self, request: flask.Request, request_id: str | None = None
+    ) -> flask.Response:
+        forward_addr = f"http://{self.host}:{self.port}{request.path}"
+
+        request_headers = dict(request.headers)
+        request_id = request_id or str(uuid.uuid4())
+        request_headers["X-Request-ID"] = str(request_id)
+
+        forward = requests.request(
+            method=request.method,
+            url=forward_addr,
+            headers=request_headers,
+            data=request.data,
+            cookies=request.cookies,
+            allow_redirects=False,
+        )
+        return flask.Response(
+            forward.content, forward.status_code, forward.headers.items()
+        )
 
 
 @dc.dataclass
--- 104_load_balancer/lb/tests/test_lb.py
+++ 104_load_balancer/lb/tests/test_lb.py
@@ -1,14 +1,25 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
 import unittest
+from unittest import mock
 import flask
+import requests
+
+from lb import servers, strategies
+from lb.app import app as main
+
+if TYPE_CHECKING:
+    from flask.testing import FlaskClient
 
 
 def create_app() -> flask.Flask:
-    app = flask.Flask(__name__)
+    app = main.create_app()
     app.config.update({"TESTING": True})
     return app
 
 
-def get_client(app: flask.Flask | None = None) -> flask.testing.FlaskClient:
+def get_client(app: flask.Flask | None = None) -> FlaskClient:
     if app is None:
         app = create_app()
     return app.test_client()
@@ -16,4 +27,21 @@ def get_client(app: flask.Flask | None = None) -> flask.testing.FlaskClient:
 
 class TestForwarding(unittest.TestCase):
     def setUp(self) -> None:
-        self.client = get_client()
+        app = create_app()
+        self.client = get_client(app)
+
+        _servers = (servers.BasicServer(host="test", port=8000),)
+        self.strategy = strategies.RoundRobin.new(_servers)
+
+    @mock.patch("lb.servers.requests.request")
+    @mock.patch("lb.strategies.get_strategy")
+    def test_forwards_to_server(
+        self, mock_get_strategy: mock.Mock, mock_request: mock.Mock
+    ) -> None:
+        mock_get_strategy.return_value = self.strategy
+        mock_request.return_value = mock.Mock(
+            status_code=200, content=b"OK", headers={}, spec=requests.Response
+        )
+
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
