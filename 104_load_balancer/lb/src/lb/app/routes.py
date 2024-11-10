406233eb5bd55874aab8e5f6b9d4a11d80dc670c
Atsushi Toda <todaatsushi0@gmail.com>
2024-11-10 13:07:14 +0000

    Forwarding if unresponsive

--- 104_load_balancer/lb/src/lb/app/routes.py
+++ 104_load_balancer/lb/src/lb/app/routes.py
@@ -4,6 +4,8 @@ import uuid
 from lb import strategies
 
 
+# Env var
+MAX_ATTEMPS = 2
 strategy = strategies.get_strategy()
 
 
@@ -11,5 +13,13 @@ def configure_routes(app: flask.Flask) -> None:
     @app.route("/")
     def lb() -> flask.Response:
         request_id = uuid.uuid4()
-        dest = strategy.get_next()
-        return dest.forward(flask.request, str(request_id))
+
+        tries = 0
+        while tries < MAX_ATTEMPS:
+            dest = strategy.get_next()
+            response = dest.forward(flask.request, str(request_id))
+            if response.status_code >= 500:
+                tries += 1
+                continue
+            return response
+        return response
--- 104_load_balancer/lb/tests/test_lb.py
+++ 104_load_balancer/lb/tests/test_lb.py
@@ -25,23 +25,45 @@ def get_client(app: flask.Flask | None = None) -> FlaskClient:
     return app.test_client()
 
 
+def create_strategy(
+    klass: type[strategies.Strategy], num_servers: int
+) -> strategies.Strategy:
+    _servers = tuple(
+        servers.BasicServer(host="local", port=1 + i) for i in range(num_servers)
+    )
+    return klass.new(_servers)
+
+
 class TestForwarding(unittest.TestCase):
     def setUp(self) -> None:
         app = create_app()
         self.client = get_client(app)
-
-        _servers = (servers.BasicServer(host="test", port=8000),)
-        self.strategy = strategies.RoundRobin.new(_servers)
 
     @mock.patch("lb.servers.requests.request")
     @mock.patch("lb.strategies.get_strategy")
     def test_forwards_to_server(
         self, mock_get_strategy: mock.Mock, mock_request: mock.Mock
     ) -> None:
-        mock_get_strategy.return_value = self.strategy
+        mock_get_strategy.return_value = create_strategy(strategies.RoundRobin, 1)
         mock_request.return_value = mock.Mock(
             status_code=200, content=b"OK", headers={}, spec=requests.Response
         )
+
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    @mock.patch("lb.servers.requests.request")
+    @mock.patch("lb.strategies.get_strategy")
+    def test_forwards_to_next_if_bad_response(
+        self, mock_get_strategy: mock.Mock, mock_request: mock.Mock
+    ) -> None:
+        mock_get_strategy.return_value = create_strategy(strategies.RoundRobin, 1)
+        mock_request.side_effect = [
+            mock.Mock(status_code=503, content=b"", headers={}, spec=requests.Response),
+            mock.Mock(
+                status_code=200, content=b"ok", headers={}, spec=requests.Response
+            ),
+        ]
 
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
